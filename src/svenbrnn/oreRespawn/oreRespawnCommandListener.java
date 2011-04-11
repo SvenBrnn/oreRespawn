39173a60334afe789cce168b8cc7f4e008bcf2b7
SvenBrnn <svenbrnn@web.de>
2011-04-11 17:33:34 +0200

    First Changes for Command and Permission Support

--- src/svenbrnn/oreRespawn/oreRespawnCommandListener.java
+++ src/svenbrnn/oreRespawn/oreRespawnCommandListener.java
@@ -2,7 +2,6 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package svenbrnn.oreRespawn;
 
 import com.nijiko.permissions.PermissionHandler;
@@ -16,6 +15,7 @@ import org.bukkit.plugin.Plugin;
  * @author Sven
  */
 public class oreRespawnCommandListener {
+
     private Plugin plugin;
     private oreRespawnRespawner respawn;
     private oreRespawnConfig config;
@@ -30,19 +30,21 @@ public class oreRespawnCommandListener {
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         boolean ret = false;
         String cmd = command.getName().toLowerCase();
-        Player pl = (Player)sender;
-        if(cmd.equals("ores"))
-        {
-            if(args[0].endsWith("spawnnow"))
-            {
-                respawn.spawnNow();
-                pl.sendMessage("[oreRespawn] Respawning all Ores!");
+        Player pl = (Player) sender;
+        if (cmd.equals("ores")) {
+            if (args[0].endsWith("spawnnow")) {
+                if (Permissions.has(pl, "orerespawn.respawn")) {
+                    respawn.spawnNow();
+                    pl.sendMessage("[oreRespawn] Respawning all Ores!");
+                    ret = true;
+                }
+            } else {
+                if (Permissions.has(pl, "orerespawn")) {
+                    pl.sendMessage("[oreRespawn] Parameter List:");
+                    pl.sendMessage("spawnnow: Spawns all Ore thats not spawned yet");
+                    ret = true;
+                }
             }
-            else {
-                pl.sendMessage("[oreRespawn] Parameter List:");
-                pl.sendMessage("spawnnow: Spawns all Ore thats not spawned yet");
-            }
-            ret = true;
         }
         return ret;
     }
